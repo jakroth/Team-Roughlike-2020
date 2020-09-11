ebfd02a774852c5ed76fed164205e3e32533f110
chillehh <jacobrling@gmail.com>
2020-09-11 09:33:54 +0930

    Added a static TextboxController
    The TextboxController script allows for anyone to push any form of text to the textbox whenever they want the game to say something or give a hint etc. It does this by calling TextboxController.UpdateText()

---
+++ DemonSchool/Assets/Scripts/UI/TextboxController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TextboxController : MonoBehaviour
+{
+    public static TextboxController instance;
+    public static TextMeshProUGUI textboxText;
+
+    void Awake()
+    {
+        instance = this;
+        textboxText = GameObject.FindGameObjectWithTag("Textbox").GetComponent<TextMeshProUGUI>();
+    }
+
+    public static void UpdateText(string txt)
+    {
+        textboxText.text = txt;
+    }
+}
