e63d135608d611b64d08c29497f75e635b6c300d
li1693 <li1693@flinders.edu.au>
2020-10-31 15:17:10 +1030

    Animation add and sprite change
    :)

--- DemonSchool/Assets/Scripts/Player/fireBall.cs
+++ DemonSchool/Assets/Scripts/Player/fireBall.cs
@@ -7,27 +7,30 @@ public class fireBall : MonoBehaviour
 {
     public float speed;//Setting up Fireball's fly speed(can be reset in Unity's UI)
     public Rigidbody2D rb;//Setting up Fireball's Rigidbody
+    public Sprite touch;
 
     void Start()
     {
         rb.velocity = transform.right * speed;//fireball fly status
 
-        while (speed != 0)
-        {
-         
-        }
-
         Destroy(gameObject, 1);
+    }
+
+    private void Update()
+    {
+        this.transform.Rotate(1, 1, speed);
     }
 
     private void OnTriggerEnter2D(Collider2D hit)
     {
         if (hit.tag == "enemy")
         {
+            //this.GetComponent<SpriteRenderer>().sprite = touch;
             Destroy(gameObject);
         }
         else if (hit.tag == "map")
         {
+           // this.GetComponent<SpriteRenderer>().sprite = touch;
             Destroy(gameObject);
         }
       
